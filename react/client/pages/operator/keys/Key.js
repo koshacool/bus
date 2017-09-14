93580952ae040d2acaa060f5477dc959aa47f465
roman <roman.kushytskyy@gmail.com>
2017-09-14 11:13:11 +0300

    add ability to change hot keys

--- react/client/pages/operator/keys/Key.js
+++ react/client/pages/operator/keys/Key.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { keyCodes } from '../params';
 
+const Key = ({actionObj, actionName, onEdit, inEdding}) => {
+  const onClick = () => onEdit(actionName, actionObj.code);
+  const keyValue = keyCodes[actionObj.code];
 
-const Key = ({actionObj}) => {
-  console.log(actionObj);
   return (
     <div className="key-desc">
       <div className="key__wrap">
-        <button>{String.fromCharCode(actionObj.code)}</button>
+        <button onClick={onClick}>
+          { inEdding === actionObj.code ? 'Press key' : keyValue }
+        </button>
       </div>
       <div>{actionObj.text}</div>
+      <hr/>
     </div>
   );
 };
 
 Key.propTypes = {
   actionObj: PropTypes.object.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  inEdding: PropTypes.string.isRequired,
+  actionName: PropTypes.string.isRequired,
 };
 
 export default Key;
-
-//
-// function getChar(e) {
-//   console.log(e)
-//   if (e.key === '' || e.key === ' ') {
-//     return e.code;
-//   }
-//
-//   return e.key;
-// };
--- react/client/pages/operator/keys/Keys.js
+++ react/client/pages/operator/keys/Keys.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {Row, Input} from 'react-materialize';
-import {defaultKeys} from '../params';
+import { defaultKeys } from '../params';
 
 import Key from './Key';
 
@@ -11,27 +9,73 @@ class Keys extends React.Component {
 
     this.state = {
       hotKeys: defaultKeys,
+      inEdding: '',
+      actionName: '',
     };
 
     this.renderKeys = this.renderKeys.bind(this);
+    this.onEdit = this.onEdit.bind(this);
+    this.onKeydown = this.onKeydown.bind(this);
   }
 
   componentDidMount() {
     // Get keys from DB
   }
 
+  onEdit(actionName, keyCode) {
+    this.setState({inEdding: keyCode, actionName});
+    document.addEventListener('keydown', this.onKeydown);
+  }
+
+  onKeydown(event) {
+    if (!event.metaKey) {
+      event.preventDefault();
+    }
+
+    const { inEdding } = this.state;
+    const keyCode = event.keyCode || event.which;
+
+    if (!inEdding) {
+      return;
+    }
+
+    this.setState({ inEdding: '', actionName: '', hotKeys: this.changeActionKeys(keyCode) });
+    document.removeEventListener('keydown', this.onKeydown);
+  }
+
+  changeActionKeys(keyCode) {
+    const {hotKeys, inEdding, actionName} = this.state;
+    const actions = Object.keys(hotKeys);
+
+    if (keyCode != inEdding) {
+      actions.map(action => {
+        if (hotKeys[action].code == keyCode) {
+          hotKeys[action].code = inEdding;
+        }
+      });
+
+      hotKeys[actionName].code = `${keyCode}`;
+    }
+
+    return hotKeys;
+  }
+
   renderKeys() {
-    const {hotKeys} = this.state;
+    const { hotKeys, inEdding } = this.state;
     const actions = Object.keys(hotKeys);
 
     return actions.map(action => (
-      <Key key={hotKeys[action].code} actionObj={hotKeys[action]}/>
+      <Key
+        key={hotKeys[action].code}
+        actionName={action}
+        actionObj={hotKeys[action]}
+        onEdit={this.onEdit}
+        inEdding={inEdding}
+      />
     ));
   }
 
-
   render() {
-
     return (
       <div className="container key__list">
         {this.renderKeys()}
